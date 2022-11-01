21c8395174323ad28dd675fc27bc6dbc7d389236
lja015 <79137294+lja015@users.noreply.github.com>
2022-11-01 16:39:06 +0530

    Create odd even.py

---
+++ basicprgm1/odd even.py
@@ -0,0 +1,8 @@
+num=int(input("Enter a number"))
+if num%2==0:
+   if num>2:
+       print("num is greater than 2 and even")
+   elif num<2:
+       print("num is less than 2 ")
+else:
+  print("no is odd")
